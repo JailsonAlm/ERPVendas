437cb8b7427d550107ce8548683440b9353d7f9f
JailsonAlm <jailson.07almeida@gmail.com>
2022-09-06 23:50:56 -0300

    first commit

---
+++ src/main/java/sistema/gerencial/ERPVendas/ErpVendasApplication.java
@@ -0,0 +1,13 @@
+package sistema.gerencial.ERPVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema/gerencial/ERPVendas/ErpVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.gerencial.ERPVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
